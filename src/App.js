e8b8b513656ed6f8eeb618803f439fba2750e8d0
Miguel Ferre <miguelferreporcel@gmail.com>
2023-03-05 19:07:01 +0100

    back to origin

--- src/App.js
+++ src/App.js
@@ -1,40 +1,7 @@
-// Importa las funciones que definen las páginas
-import { HomePage, NotFoundPage, PostFormPage } from './pages'
-
-// Importa Componentes Routes y Route para definir y crear rutas
-import { Routes, Route } from 'react-router-dom'
-
-/* Importa el componente PostProvider, cada objeto Context viene con un componente Provider de React que permite que los componentes que lo consumen se suscriban a los cambios del contexto. */
-import { PostProvider } from './context/postContext'
-
-// Importa el componente Toaster empleado para implementar notificaciones al realizar acciones
-import { Toaster } from 'react-hot-toast'
-
-
-
-// Definición de rutas de todas las páginas
-function App() {
+export default function App() {
   return (
-    <div className='bg-neutral-900 min-h-screen flex items-center'>
-      <div className='px-10 container m-auto'>
-        <PostProvider>
-
-          {/* react pide que las rutas estén dentro de un <BrowserRouter /> esto se hace en index.js 
-            * <BrowserRouter> <App /> </BrowserRouter>
-          */}
-          <Routes>
-            <Route path = '/' element = { <HomePage />} />
-            <Route path = '/new' element = { <PostFormPage />} />
-            <Route path = '/posts/:id' element = { <PostFormPage />} />
-            <Route path = '*' element = { <NotFoundPage />} />
-          </Routes>
-
-          {/* Notificación en principio oculta */}
-          <Toaster />
-        </PostProvider>        
-      </div>      
-    </div>    
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
-
-export default App;
--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * ordenar y exportar las funciones de las páginas del proyecto que serán llamadas por App.js ->
- * -> "import { HomePage, NotFoundPage, PostFormPage } from './pages'" es decir: ./pages/index.js
- */
-export { HomePage } from './HomePage'
-export { NotFoundPage } from './NotFoundPage'
-export { PostFormPage } from './PostFormPage'
