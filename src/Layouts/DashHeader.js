e58fa48fc87eae715c161c567dc474a9d9761cf5
Miguel Ferre <miguelferreporcel@gmail.com>
2023-04-17 11:25:25 +0200

    DashHeader

--- src/Layouts/DashHeader.js
+++ src/Layouts/DashHeader.js
@@ -9,11 +9,11 @@ export function DashHeader() {
         <span className="font-bold  text-2xl tracking-tight">alikindoiPhoto</span>
       </div>
       <div className="w-full block justify-flex-grow lg:flex lg:items-center lg:w-auto">
-        <div className="text-xl text-white font-semibold lg:flex-grow">
-          <Link className="mr-10" to='/login'>Login </Link>
-          <Link className="mr-10" to='/logout'>Logout </Link>
-          <Link className="mr-10" to='/links'>Links </Link>
-          <Link className="mr-10" to='/dash/admin'>Admin </Link>
+        <div className="text-xl text-white font-semibold lg:flex-grow ">
+          <Link className="mr-7 ml-4" to='/login'>Login </Link>
+          <Link className="mr-7 ml-0" to='/logout'>Logout </Link>
+          <Link className="mr-7" to='/links'>Links </Link>
+          <Link to='/dash/admin'>Admin </Link>
         </div>
       </div>
     </nav>
